62fc74c5a4d0fa9ff47896f7179dceee6ea583a4
DoMinhQuan <doquan23032003@gmail.com>
2023-12-10 15:07:21 +0700

    Config .env and config.js to deploy

--- config/db/index.js
+++ config/db/index.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config()
                        
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORDS, {
-      host: `${process.env.HOST}`,
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
+      host: process.env.DATABASE_HOST,
       dialect: 'mysql',
       logging: false,
       createdAt: false, 
